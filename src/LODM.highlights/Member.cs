c4958278844468b77480a7428f716ec8f46f1f46
Charlie Hust <chust@iwthealth.com>
2016-06-10 14:59:42 -0500

    removing unusued refs.

--- src/LODM.highlights/Member.cs
+++ src/LODM.highlights/Member.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace LODM.highlights
+﻿namespace LODM.highlights
 {
     public class Member
     {
